a16953831639a655f052aa548021471ca5c8d585
ayushatindra <116113390+ayushatindra@users.noreply.github.com>
2024-01-07 01:41:03 +0530

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,226 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a=5;
+    // int b=3;
+    // cout<<(a<=b)<<endl;
+
+    // cout << !(a==5);
+
+    // cout<<(5)/((double)3);
+
+    // int a = 5;
+    // int b = 3;
+
+    // cout << a+b <<endl;
+    // cout << a-b <<endl;
+    // cout << a*b <<endl; 
+    // cout << a%b <<endl;
+    // cout << a%b  <<endl;
+
+    // double d=5.7;
+    // int x =(double)d;
+    // cout<<"value of x is"<<x<<endl;
+
+    // char ch = 97;
+    // cout << ch <<endl;
+
+    // int num ='b';
+    // cout << num <<endl;
+
+    //  long long a = 5;
+    //  cout << sizeof(a)<<endl;
+
+    // int a = 5;
+    // cout << "Value of a is: "<<a<<endl;
+  
+    //  char ch = 'A';
+    //  cout <<"value of ch is "<< ch << endl;
+
+    // bool flag = 0;
+    // cout<<"value of ch is :"<<flag;
+    
+    // // cout<<"Nameste Bharat\n";
+     
+    //  cout<<"Enter a number:"<<endl;
+
+    // int a;
+    // //taking input
+    // cin>>a;
+    // cout<<"You entered "<<a<<endl;
+
+    //class 3
+    //decler a integer type variable
+    // int age;
+    // //print enter the score
+    // cout<<"Enter the age"<<endl;
+    // //take input into score variable
+    // cin>>age;
+     
+    //  //if condition 
+    //  if(age >= 18){
+    //     cout << "I can vote"<<endl;
+    //  }
+
+    // else{
+    //     cout<<"I cannot vote"<<endl;
+    // }
+
+//     int mark;
+//     cin>>mark;
+//     //nested if else
+//     if(mark >= 90 ){
+//         cout<<"A grad";
+//     }
+//     else if(mark >= 80){
+//         cout<<"B grad ";
+//     }
+//     else if(mark >= 60){
+//         cout<<"c grad";
+//     }
+//     else if(mark >=40){
+//         cout<<"D grad";
+//     }
+
+// else{
+//     cout<<"F grad";
+// // }
+//     int mark;
+//     cin>>mark;
+//     //nested if else
+//     if(mark >= 90 ){
+//         cout<<"A grad";
+//     }
+//     else if(mark >= 80){
+//         cout<<"B grad ";
+//     }
+//     else if(mark >= 60){
+//         cout<<"c grad";
+//     }
+//     else if(mark >=40){
+//         cout<<"D grad";
+//     }
+
+// else{
+//     cout<<"F grad";
+// }
+
+
+
+//    for(int i=0 ;i< 5;i++){
+//     cout<<"Ayush Atindra"<<endl;
+//    }
+   
+//    for(int i=0; i<5; i++){
+//     cout<<i<<endl;
+//    }
+    //  for(int i=5;i>0;i--){
+    //     cout<<i<<endl;
+    //  }
+    
+
+    // for(int i=0;i<=10;i++){
+    //     cout<<2*i<<endl;
+    // }
+    
+    // for(int i=0;i<=5;i+=2){
+    //   cout<<i<<endl;
+    // }
+
+    // for(int i=1;i<10;i=i*4){
+    //     cout<< "Ayush "<<i <<endl;
+    // }
+
+
+    // for(int i=1 ;i<10 ;i++){
+    //     cout<< i <<endl;
+    // }
+
+    // for(int i=100;i>0;i=i/2){
+    //     cout<<"ayush "<<i<<endl;
+    // }
+
+    // for(int i=5; (i>=0 && i<=10); i++){
+    //     cout<< i <<endl;
+    // }
+
+    //  int i=0 ;
+    //  for(;;) {
+    //     if(i<10){
+    //         cout<<i;
+    //         i++;
+    //     }
+        
+//     //  }
+//   for(int i= 0;i<10 ;i++){
+//     cout << i << endl;
+//   }
+
+//  cout<<"Ayush"<<endl;
+
+ //Printing Solid rectangle
+//  for(int row=0;row<5;row++){
+//     for(int col=0;col<5;col++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+//  }
+ //hollw rectangle
+//  for(int row=0;row<5;row++){
+//     if(row==0|| row==4){
+//     for(int col=0;col<5;col++){
+//         cout<<"*";
+//     }
+//  }
+//  else{
+//     cout<<"*";
+//     for(int i=0;i<3;i++){
+//         cout<<" ";
+
+//     }
+//     cout<<"*";
+//  }
+//  cout<<endl;
+//  }
+
+// int n;
+// cin>>n;
+
+
+//  for(int row=0;row<n;row++){
+//     if(row==0|| row==n-1){
+//     for(int col=0;col<n;col++){
+//         cout<<"*";
+//     }
+//  }
+//  else{
+//     cout<<"*";
+//     for(int i=0;i<n-2;i++){
+//         cout<<" ";
+
+//     }
+//     cout<<"*";
+// //  }
+//  cout<<endl;
+//  }
+
+// 
+// int n;
+// cin>>n;
+// for(int row=0;row<n;row++){
+//     for(int col=0;col<row+1;col++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+// }
+
+int n;
+cin >> n;
+for(int row=0;row<n;row++){
+    for(int col=0;col<n-row;col++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+}
